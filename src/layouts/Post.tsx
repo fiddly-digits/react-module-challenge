e8dee345cc997bbe6e2a6755feb3c07198a5bd3f
fiddly-digits <rob.crlu@gmail.com>
2023-07-09 16:48:12 -0600

    feat: creation of post layout

---
+++ src/layouts/Post.tsx
@@ -0,0 +1,12 @@
+export default function Post() {
+  return (
+    <>
+      <nav></nav>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-12 grid-rows-3 gap-4'>
+          <div className='grid grid-cols-8 grid-start-2 border-helper'></div>
+        </div>
+      </div>
+    </>
+  );
+}
